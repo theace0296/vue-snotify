302bb63f9d2977c6f2803182d8925578bf1f1278
Chase <29675446+theace0296@users.noreply.github.com>
2022-12-30 17:04:34 -0500

    prep for publish

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,10 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 import copyAndTranspileSass from './plugins/copy-and-transpile-sass';
 
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'vue-snotify',
@@ -22,6 +24,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    dts({ insertTypesEntry: true }),
     copyAndTranspileSass({
       entry: path.resolve(__dirname, 'src'),
       include: [/styles\/\w+\.scss$/]
